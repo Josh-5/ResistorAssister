bbd982c5aae9461e4bdddeec8c234b09c2a996c9
Josh Pryor <josh24@vt.edu>
2022-02-26 15:17:25 -0500

    added functionality to combine()

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ import sys
 with open('resistors.txt') as resistorFile:
     singleResistors = []
     for line in resistorFile:
-         singleResistors.append([int(x) for x in line.split()])
+        singleResistors.append([int(line), line])
 
 
-rCombo = ""
 rTarget = sys.argv[0]
 rCount = sys.argv[1]
-closestR = 0
+rClosest = []
 
 
 def series(r1, r2):
@@ -21,35 +20,44 @@ def parallel(r1, r2):
     return (r1 * r2)/(r1 + r2)
 
 
-# TODO: rename target or rTarget to better differentiate them
-def find_closest(target, r1List, r2List):
-    result = [series(r1List[0], r2List[0]), str(r1List[0]) + ' + ' + str(r2List)]
-    for r1 in r1List:
-        for r2 in r2List:
-            if (series(r1, r2) - target) < (result[0] - target):
-                result[0] = series(r1, r2)
-                result[1] = str(r1) + ' + ' + str(r2)
-            if (parallel(r1, r2) - target) < (result[0] - target):
-                result[0] = parallel(r1, r2)
-                result[1] = str(r1) + ' || ' + str(r2)
+def find_closest(r1_list, r2_list):
+    closest = [series(r1_list[0], r2_list[0]), str(r1_list[0]) + ' + ' + str(r2_list)]
+    global rTarget
+    for r1 in range(len(r1_list)):
+        for r2 in range(len(r2_list)):
+            r1Val = r1[1]
+            r2Val = r2[1]
+            if (series(r1Val, r2Val) - rTarget) < (closest[0] - rTarget):
+                closest[0] = series(r1Val, r2Val)
+                closest[1] = '(' + str(r1Val) + ' + ' + str(r2Val) + ')'
+            if (parallel(r1, r2) - rTarget) < (closest[0] - rTarget):
+                closest[0] = parallel(r1, r2)
+                closest[1] = '(' + str(r1) + ' || ' + str(r2) + ')'
     return result
 
 
-# #tracks the current combination possibilities through the recursion
-# comboResistors = singleResistors
-#
-# # finds the closest combination of resistors using recursion
-# # takes two array arguments containing the possibilities for each resistor
-# def combine(r2):
-#     if rCount == 2:
-#         isSeries = True
-#         for r1 in singleResistors:
-#             if (series(r1, r2) - rTarget) < (series(r1, r2) - rTarget)
-#     else:
-#         for r1 in singleResistors:
-#             for r2 in comboResistors:
-#                 if !(series(r1, r2) in comboResistors):
-#                     comboResistors.append(series(r1, r2))
-#
-#                 if !(parallel(r1, r2) in comboResistors):
-#                     comboResistors.append(parallel(r1, r2))
+# finds the closest combination of resistors using recursion
+# takes two array arguments containing the possibilities for each resistor
+def combine(combo_resistors = singleResistors):
+    global rCount
+    global rTarget
+    if rCount == 2:
+        return find_closest(singleResistors, combo_resistors)
+    else:
+        new_combos = []
+        for r1 in range(len(singleResistors)):
+            for r2 in range(len(combo_resistors)):
+                r1Val = r1[1]
+                r2Val = r2[1]
+                if series(r1Val, r2Val) not in singleResistors:
+                    new_combos.append([series(r1, r2), '(' + str(r1) + ' + ' + str(r2) + ')'])
+                if parallel(r1Val, r2Val) not in new_combos:
+                    new_combos.append([parallel(r1, r2), '(' + str(r1) + ' || ' + str(r2) + ')'])
+        rCount -= 1
+        combine(new_combos)
+
+
+# output
+result = combine()
+print('best combination: ' + str(result[1]))
+print('total resistance: ' + str(result[0]))
